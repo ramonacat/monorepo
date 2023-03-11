b72311da2d9a9811326773da4956f1bee52a9378
Ramona Łuczkiewicz <ja@agares.info>
2023-03-11 14:11:34 +0100

    Reconfigure monitors

--- users/ramona_gui.nix
+++ users/ramona_gui.nix
@@ -52,12 +52,11 @@
           modifier = "Mod4";
           output = {
             "Dell Inc. DELL U2720Q JKPQT83" = {
-              scale = "1";
+              scale = "2";
               pos = "1920 0";
-              mode = "1920x1080";
             };
-            "Dell Inc. DELL U2415 7MT0186418CU" = { pos = "0 0"; };
-            "Dell Inc. DELL U2415 XKV0P9C334FS" = { pos = "3840 0"; };
+            "Dell Inc. DELL U2415 7MT0186418CU" = { pos = "3840 0"; };
+            "Dell Inc. DELL U2415 XKV0P9C334FS" = { pos = "0 0"; };
           };
           bars = [
             {
