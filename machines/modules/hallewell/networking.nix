ddb163c3c6f2b81ef2579c14a0ce552619672f7d
Ramona Łuczkiewicz <ja@agares.info>
2023-03-16 12:18:24 +0100

    Bring static IP assignments back

--- machines/modules/hallewell/networking.nix
+++ machines/modules/hallewell/networking.nix
@@ -2,7 +2,17 @@
 {
   config = {
     networking.hostName = "hallewell";
-    networking.interfaces.br0.useDHCP = true;
+
+    networking.useDHCP = lib.mkForce false;
+    networking.interfaces.br0.useDHCP = lib.mkForce false;
+    networking.interfaces.eno1.useDHCP = lib.mkForce false;
+
+    networking.interfaces.br0.ipv4.addresses = [
+      { address = "10.69.10.5"; prefixLength = 24; }
+    ];
+
+    networking.defaultGateway = "10.69.10.1";
+    networking.nameservers = [ "8.8.8.8" ];
 
     networking.bridges = {
       "br0" = {
