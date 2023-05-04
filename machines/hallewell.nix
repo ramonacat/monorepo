0cf353d4c7a034bf908693098ee2d7ba0b6d970b
Ramona Łuczkiewicz <ja@agares.info>
2023-05-04 23:23:06 +0200

    Add syncthing

--- machines/hallewell.nix
+++ machines/hallewell.nix
@@ -6,5 +6,26 @@
         device = "10.69.10.139:/mnt/data0/data";
         fsType = "nfs";
       };
+
+    services.syncthing = {
+      enable = true;
+      overrideDevices = true;
+      overrideFolders = true;
+
+      devices = {
+        "phone" = { "id" = "VZK66I4-WTFCOWJ-B7LH6QV-FDQFTSH-FTBWTIH-UUDRUOR-SNIZBPS-AMRDBAU"; };
+      };
+
+      folders = {
+        "shared" = {
+          path = "/mnt/nas/shared/";
+          devices = [ "phone" ];
+        };
+      };
+    };
+
+    # For syncthing
+    networking.firewall.allowedTCPPorts = [ 22000 ];
+    networking.firewall.allowedUDPPorts = [ 22000 21027 ];
   };
 }
