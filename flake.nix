dd3df05b7f9576bb4669b84965cc72744ebf5298
Ramona Łuczkiewicz <ja@agares.info>
2023-08-06 20:52:52 +0200

    Cleanup hallewell

--- flake.nix
+++ flake.nix
@@ -19,15 +19,10 @@
             ./machines/modules/base.nix
             ./machines/modules/bcachefs.nix
             ./machines/modules/installed_base.nix
-            ./machines/modules/workstation.nix
             ./users/ramona.nix
-            ./users/ramona_gui.nix
             home-manager.nixosModules.home-manager
             ./machines/modules/hallewell/hardware.nix
-            ./machines/modules/hallewell/virtualisation.nix
             ./machines/modules/hallewell/networking.nix
-            ./machines/modules/hallewell/home-assistant.nix
-            ./machines/modules/hallewell/users/ramona_gui.nix
             ./machines/hallewell.nix
           ];
         };
@@ -43,7 +38,7 @@
             ./machines/modules/moonfall/hardware.nix
             ./machines/modules/moonfall/networking.nix
             ./machines/modules/moonfall/virtualisation.nix
-            ./machines/modules/hallewell/users/ramona_gui.nix
+            ./machines/modules/moonfall/users/ramona_gui.nix
             ./machines/moonfall.nix
           ];
         };
