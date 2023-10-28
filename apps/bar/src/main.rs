a52d9153b936effc7ec27af21314a4848ba36023
Ramona Łuczkiewicz <ja@agares.info>
2023-10-29 00:11:43 +0200

    Don't break if upower is not available

--- apps/bar/src/main.rs
+++ apps/bar/src/main.rs
@@ -55,20 +55,45 @@ fn get_pa_mute() -> bool {
     false
 }
 
+struct BatteryState {
+    is_on_battery: bool,
+    percent: f64
+}
+
+async fn get_battery_state(upower: Option<&UPowerProxy<'_>>) -> Option<BatteryState> {
+    match upower {
+        Some(ref x) => {
+            let Ok(is_on_battery) = x.on_battery().await else { 
+                return None; 
+            };
+
+            let Ok(display_device) = x.get_display_device().await else {
+                return None;
+            };
+
+            let Ok(percent) = display_device.percentage().await else {
+                return None;
+            };
+
+            Some(BatteryState{
+                is_on_battery,
+                percent
+            } )
+        }
+        None => None
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let connection = zbus::Connection::system().await.unwrap();
-    let upower = UPowerProxy::new(&connection).await.unwrap();
+    let upower = UPowerProxy::new(&connection).await.ok();
 
     println!("{}", "{\"version\": 1, \"click_events\": false}");
     println!("[");
 
     loop {
-        let is_on_battery = upower.on_battery().await.unwrap();
-        let battery = upower.get_display_device().await.unwrap();
-        let battery_percent = battery.percentage().await.unwrap();
-        let battery_emoji = if is_on_battery { "🔋" } else { "🔌" };
-
+        let battery_state = get_battery_state(upower.as_ref()).await;
         let volume = get_pa_volume();
         let mute_emoji = if get_pa_mute() { "🔇" } else { "🔊" };
         let now = chrono::Local::now();
@@ -78,22 +103,27 @@ async fn main() {
             libc::getloadavg(loadavg.as_mut_ptr(), 2);
         }
 
+        let mut blocks = vec![];
+        if let Some(battery_state) = battery_state {
+            let battery_emoji = if battery_state.is_on_battery { "🔋" } else { "🔌" };
+            blocks.push(Block {
+                full_text: format!("{} {:.0}%", battery_emoji, battery_state.percent)
+            })
+        }
+
+        blocks.push(Block {
+            full_text: format!("{} {:.0}%", mute_emoji, volume * 100.0f64)
+        });
+        blocks.push( Block {
+            full_text: format!("🏋 {:.2} {:.2}", loadavg[0], loadavg[1])
+        });
+        blocks.push(Block {
+            full_text: now.format("🕛 %Y-%m-%d %H:%M").to_string()
+        });
+
         println!(
             "{},",
-            serde_json::to_string(&[
-                Block {
-                    full_text: format!("{} {:.0}%", battery_emoji, battery_percent)
-                },
-                Block {
-                    full_text: format!("{} {:.0}%", mute_emoji, volume * 100.0f64)
-                },
-                Block {
-                    full_text: format!("🏋 {:.2} {:.2}", loadavg[0], loadavg[1])
-                },
-                Block {
-                    full_text: now.format("🕛 %Y-%m-%d %H:%M").to_string()
-                },
-            ])
+            serde_json::to_string(&blocks)
             .unwrap()
         );
 
