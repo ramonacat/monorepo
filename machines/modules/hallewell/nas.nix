2f700f844e96fc16031997b231c9c0d7097b9610
Ramona Łuczkiewicz <ja@agares.info>
2023-11-08 22:48:17 +0100

    Dark magic for hardware transcoding on hallewell

--- machines/modules/hallewell/nas.nix
+++ machines/modules/hallewell/nas.nix
@@ -43,5 +43,20 @@
         };
       };
     };
+
+    # Dark magic for transcoding acceleration
+    nixpkgs.config.packageOverrides = pkgs: {
+      vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+    };
+    hardware.opengl = {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver
+        vaapiIntel
+        vaapiVdpau
+        libvdpau-va-gl
+        intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      ];
+    };
   };
 }
