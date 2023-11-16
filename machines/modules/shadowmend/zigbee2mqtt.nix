3ae88c7f5112e208c3a53814594d2e86c2f62d5f
Ramona Łuczkiewicz <ja@agares.info>
2023-11-16 20:38:02 +0100

    "Configure smooth zigbee network transition"

--- machines/modules/shadowmend/zigbee2mqtt.nix
+++ machines/modules/shadowmend/zigbee2mqtt.nix
@@ -11,6 +11,12 @@
         serial = {
           port = "/dev/ttyACM0";
         };
+        advanced = {
+          pan_id = 30007;
+          ext_pan_id = [160 113 48 240 62 4 244 87];
+          channel = 15;
+          network_key = [89 16 49 175 172 52 209 203 83 191 57 2 24 168 219 71];
+        };
       };
     };
 
