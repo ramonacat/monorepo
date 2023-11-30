0c992373e73b6eb2588abd5a73853298dcc099f8
Ramona Łuczkiewicz <ja@agares.info>
2023-11-30 17:19:53 +0100

    "Script for updating all machines"

--- machines/moonfall.nix
+++ machines/moonfall.nix
@@ -38,5 +38,21 @@
     # For syncthing
     networking.firewall.allowedTCPPorts = [ 22000 ];
     networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+
+    environment.systemPackages = [
+      (pkgs.writeShellScriptBin "rebuild-all-machines" ''
+        #!/usr/bin/env bash
+
+        set -euo pipefail
+        set -x
+
+        sudo nixos-rebuild switch --flake .#moonfall -L --show-trace
+
+        nixos-rebuild --target-host ramona@ananas --use-remote-sudo switch --flake .#ananas -L --show-trace
+        nixos-rebuild --target-host ramona@hallewell --use-remote-sudo switch --flake .#desktop -L --show-trace
+        nixos-rebuild --target-host ramona@shadowmend --use-remote-sudo switch --flake .#shadowmend -L --show-trace
+        nixos-rebuild --target-host ramona@angelsin --use-remote-sudo switch --flake .#angelsin -L --show-trace
+      '')
+    ];
   };
 }
