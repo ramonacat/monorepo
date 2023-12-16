a2f1c3f5b58fe289f6e9618e6cd25fb04ef1e2e0
Ramona Łuczkiewicz <ja@agares.info>
2023-12-16 19:05:39 +0100

    "moonfall: black wallpaper"

--- machines/moonfall/users/ramona_gui.nix
+++ machines/moonfall/users/ramona_gui.nix
@@ -15,32 +15,32 @@
           output = {
             "Dell Inc. DELL U2415 7MT0186418CU" = {
               pos = "0 0";
-              bg = "/dev/null fill #E40303";
+              bg = "/dev/null fill #000000";
             };
             "LG Electronics LG ULTRAFINE 302MAUAEEJ14" = {
               scale = "1.5";
               pos = "1920 0";
-              bg = "/dev/null fill #FF8C00";
+              bg = "/dev/null fill #000000";
             };
             "Dell Inc. DELL U2415 XKV0P9C334FS" = {
               pos = "4480 0";
-              bg = "/dev/null fill #FFED00";
+              bg = "/dev/null fill #000000";
             };
 
             "Dell Inc. DELL U2723QE HRJH2P3" = {
               scale = "1.5";
               pos = "0 1440";
-              bg = "/dev/null fill #008026";
+              bg = "/dev/null fill #000000";
             };
             "Dell Inc. DELL U2720Q JKPQT83" = {
               scale = "1.5";
               pos = "2560 1440";
-              bg = "/dev/null fill #24408E";
+              bg = "/dev/null fill #000000";
             };
             "Dell Inc. DELL U2723QE 6MMF1P3" = {
               scale = "1.5";
               pos = "5120 1440";
-              bg = "/dev/null fill #732982";
+              bg = "/dev/null fill #000000";
             };
           };
         };
