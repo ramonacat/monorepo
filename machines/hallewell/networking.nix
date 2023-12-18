f6f31142d819e99856a28c5c4da3363afc00b142
Ramona Łuczkiewicz <ja@agares.info>
2023-12-18 15:15:10 +0100

    "hallewell+moonfall: Remove unneeded network bridges"

--- machines/hallewell/networking.nix
+++ machines/hallewell/networking.nix
@@ -4,20 +4,6 @@
     networking.hostName = "hallewell";
 
     networking.useDHCP = lib.mkForce false;
-    networking.interfaces.br0.useDHCP = lib.mkForce false;
-    networking.interfaces.eno1.useDHCP = lib.mkForce false;
-
-    networking.interfaces.br0.ipv4.addresses = [
-      { address = "10.69.10.5"; prefixLength = 24; }
-    ];
-
-    networking.defaultGateway = "10.69.10.1";
-    networking.nameservers = [ "8.8.8.8" ];
-
-    networking.bridges = {
-      "br0" = {
-        interfaces = [ "eno1" ];
-      };
-    };
+    networking.interfaces.eno1.useDHCP = lib.mkForce true;
   };
 }
--- machines/moonfall/networking.nix
+++ machines/moonfall/networking.nix
@@ -4,23 +4,9 @@
     networking.hostName = "moonfall";
 
     networking.useDHCP = lib.mkForce false;
-    networking.interfaces.br0.useDHCP = lib.mkForce false;
-    networking.interfaces.eno1.useDHCP = lib.mkForce false;
+    networking.interfaces.eno1.useDHCP = lib.mkForce true;
     networking.interfaces.eno1.wakeOnLan = {
       enable = true;
-    };
-
-    networking.interfaces.br0.ipv4.addresses = [
-      { address = "10.69.10.7"; prefixLength = 24; }
-    ];
-
-    networking.defaultGateway = "10.69.10.1";
-    networking.nameservers = [ "8.8.8.8" ];
-
-    networking.bridges = {
-      "br0" = {
-        interfaces = [ "eno1" ];
-      };
     };
   };
 }
