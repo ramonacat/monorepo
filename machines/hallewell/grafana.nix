00c70c4110e4703d45e33ee68522bea708089ba7
Ramona Łuczkiewicz <ja@agares.info>
2024-01-06 10:21:48 +0100

    "Use an FQDN for grafana so discord notifications work"

--- machines/hallewell/grafana.nix
+++ machines/hallewell/grafana.nix
@@ -7,7 +7,7 @@
         server = {
           http_addr = "0.0.0.0";
           http_port = 3000;
-          domain = "hallewell";
+          domain = "hallewell.ibis-draconis.ts.net";
         };
       };
     };
