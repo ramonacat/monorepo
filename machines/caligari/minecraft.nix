8e80fd948d6233bcab6a94c87029f8ff0f898878
Ramona Łuczkiewicz <ja@agares.info>
2024-01-09 21:40:12 +0100

    "caligari: Whitelist some humans"

--- machines/caligari/minecraft.nix
+++ machines/caligari/minecraft.nix
@@ -35,6 +35,9 @@
           whitelist = {
             Agares2 = "2535f2de-9174-4bc5-8bdf-233649bc0449";
             GayEmoPirate = "f14060b2-1b7a-436e-bb09-c7c693b4503b";
+            Franciszek_IX = "5e038352-818b-49b2-9221-7ab46e4caf15";
+            MarthaLee = "e45960c1-61cb-4a10-a20b-9bbf3926602f";
+            CleanUpGuyPL = "8a1b4570-0cc4-47ed-8cbc-a8c14e7e51ff";
           };
           serverProperties = {
             server-port = 43001;
