f231a1b7d1993e47e13c3a55ab3c0d571cc59aee
Ramona Łuczkiewicz <ja@agares.info>
2024-01-28 19:26:44 +0100

    neovim: Add a keybind to open definition in a split

--- users/ramona/neovim/extraConfig.lua
+++ users/ramona/neovim/extraConfig.lua
@@ -72,6 +72,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local opts = { buffer = ev.buf }
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gsd", ":belowright split | lua vim.lsp.buf.definition()<CR>", opts)
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
