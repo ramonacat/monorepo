0957f04882729db4a8a087f09493eac13391f404
Ramona Łuczkiewicz <ja@agares.info>
2024-02-11 14:35:25 +0100

    CI: Make the latest closures available through HTTPS
    This will allow us to later get the IDs on the machines in a cronjob
    that will pull them, so update can be fast (just switching, with copying
    already done).

--- machines/caligari.nix
+++ machines/caligari.nix
@@ -3,5 +3,8 @@
   config = {
     services.fail2ban.enable = true;
     boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+    users.users.root.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFkRskZZaMsOngUvKYgL8K6t5FBhMurjTkqbfxNLj0wE ramona@moonfall" # this is the key that's used in CI
+    ];
   };
 }
