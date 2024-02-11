87f9da3baed98c36f059681fe3eda6e766dc2631
Ramona Łuczkiewicz <ja@agares.info>
2024-02-11 16:48:06 +0100

    caligari: Allowlist own IPs in fail2ban
    While writing some scripts I managed to get banned on caligari. With
    this change the local network IPs and tailscale IPs cannot ever get
    banned.

--- machines/caligari.nix
+++ machines/caligari.nix
@@ -1,7 +1,13 @@
 { config, pkgs, lib, ... }:
 {
   config = {
-    services.fail2ban.enable = true;
+    services.fail2ban = {
+      enable = true;
+      ignoreIP = [
+        "10.69.0.0/16"
+        "100.0.0.0/8"
+      ];
+    };
     boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
     users.users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFkRskZZaMsOngUvKYgL8K6t5FBhMurjTkqbfxNLj0wE ramona@moonfall" # this is the key that's used in CI
