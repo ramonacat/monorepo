6260ce28a9bffbfd2640bbc79575c64187fe539a
Ramona Łuczkiewicz <ja@agares.info>
2024-02-16 17:53:20 +0100

    evillian: Switch to sway

--- machines/evillian.nix
+++ machines/evillian.nix
@@ -1,8 +1,4 @@
 { config, pkgs, lib, ... }:
 {
-  config = {
-    services.xserver.enable = true;
-    services.xserver.displayManager.sddm.enable = true;
-    services.xserver.desktopManager.plasma5.enable = true;
-  };
+  config = { };
 }
