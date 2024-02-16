75671b18c58b5848e77f9ae037df63002bb36bc2
Ramona Łuczkiewicz <ja@agares.info>
2024-02-16 18:22:21 +0100

    evillian: Fix greetd configuration

--- machines/evillian.nix
+++ machines/evillian.nix
@@ -1,13 +1,31 @@
 { config, pkgs, lib, ... }:
 {
   config = {
-    services.greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = "sway";
+    services.greetd =
+      let
+        swayConfig = pkgs.writeText "greetd-sway-config" ''
+          # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
+          exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+          bindsym Mod4+shift+e exec swaynag \
+            -t warning \
+            -m 'What do you want to do?' \
+            -b 'Poweroff' 'systemctl poweroff' \
+            -b 'Reboot' 'systemctl reboot'
+        '';
+      in
+      {
+        enable = true;
+        settings = {
+          default_session = {
+            command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
+          };
         };
       };
-    };
+    environment.etc."greetd/environments".text = ''
+      sway
+      nu
+      bash
+    '';
+
   };
 }
