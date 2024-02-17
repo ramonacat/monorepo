3685001ed86697ef08a5b6e0efbee6d145992742
Ramona Łuczkiewicz <ja@agares.info>
2024-02-17 15:33:30 +0100

    updater: Add a way to disable
    It might make sense in some cases, to rebuild a machine manually, just
    to test things, without committing them. With this change this can be
    achieved.

--- modules/updates.nix
+++ modules/updates.nix
@@ -13,6 +13,11 @@
             set -x
             set -euo pipefail
 
+            if [[ -f "/var/.stop_updates" ]]; then
+                echo "Updates are stopped. Remove /var/.stop_updates to reenable";
+                exit;
+            fi;
+
             CLOSURE=$(${pkgs.curl}/bin/curl "https://ramona.fun/builds/${config.networking.hostName}-closure")
             PATH="${pkgs.openssh}/bin:$PATH" NIX_SSHOPTS="-i ${config.age.secrets.universal-root.path} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ${pkgs.nix}/bin/nix-copy-closure --from root@caligari "$CLOSURE"
             ${pkgs.nix}/bin/nix-env --profile /nix/var/nix/profiles/system --set $CLOSURE
