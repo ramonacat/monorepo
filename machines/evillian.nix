ffaafadbd1285c6098b00af06c4a99ea811ab223
Ramona Łuczkiewicz <ja@agares.info>
2024-02-17 13:59:02 +0100

    evillian: Switch to LightDM

--- machines/evillian.nix
+++ machines/evillian.nix
@@ -1,9 +1,25 @@
 { config, pkgs, lib, ... }:
 {
   config = {
-    services.xserver.enable = true;
-    services.xserver.displayManager.sddm = {
+    services.xserver = {
       enable = true;
+      xkb.layout = "pl,de";
+    };
+    services.xserver.displayManager = {
+      lightdm = {
+        enable = true;
+        greeters = {
+          gtk = {
+            enable = true;
+          };
+        };
+      };
+      defaultSession = "sway";
+    };
+
+    programs.sway = {
+      enable = true;
+      wrapperFeatures.gtk = true;
     };
   };
 }
