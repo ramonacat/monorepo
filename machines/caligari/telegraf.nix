202dec13597e28108e277f5ebdb19c0149a0ce43
Ramona Łuczkiewicz <ja@agares.info>
2024-03-04 12:59:03 +0100

    caligari: Export the latest versions of closures for each host

---
+++ machines/caligari/telegraf.nix
@@ -0,0 +1,11 @@
+{...}: {
+  config = {
+    services.telegraf.extraConfig.inputs.file = {
+      files = ["/var/www/ramona.fun/builds/*-closure"];
+      data_format = "value";
+      data_type = "string";
+      name_override = "latest_closure";
+      file_tag = "filename";
+    };
+  };
+}
