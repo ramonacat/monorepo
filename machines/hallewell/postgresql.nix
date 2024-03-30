db249b8b3d80495ad0c874294abdae090f5bd568
Ramona Łuczkiewicz <ja@agares.info>
2024-03-30 14:10:57 +0100

    postgres: Reconfigure to do basebackups
    The previous wal backups were kinda busted and not really usable. Switch
    to simpler base backups every 6 hours, which don't give us PITR, but at
    least work.

--- machines/hallewell/postgresql.nix
+++ machines/hallewell/postgresql.nix
@@ -1,14 +1,21 @@
 {
   config,
   pkgs,
-  lib,
   ...
-}: {
+}: let
+  postgresPackage = pkgs.postgresql_16;
+in {
   config = {
-    age.secrets."wal-g-config.env" = {
-      file = ../../secrets/wal-g-config.env.age;
-      group = "postgres";
-      mode = "440";
+    age.secrets."hallewell-postgres-backups-rclone" = {
+      file = ../../secrets/hallewell-postgres-backups-rclone.age;
+    };
+
+    age.secrets."hallewell-postgres-backups-env" = {
+      file = ../../secrets/hallewell-postgres-backups-env.age;
+    };
+
+    age.secrets."restic-repository-password.age" = {
+      file = ../../secrets/restic-repository-password.age;
     };
 
     services.postgresql = {
@@ -16,19 +23,18 @@
       enableJIT = true;
 
       authentication = ''
-        #type database  DBuser  addresss    auth-method
-        local all       all                 trust
-        host  all       all     100.0.0.0/8 scram-sha-256
+        #type database    DBuser  addresss    auth-method
+        local all         all                 trust
+        local replication all                 trust
+        host  all         all     100.0.0.0/8 scram-sha-256
       '';
 
-      package = pkgs.postgresql_16;
+      package = postgresPackage;
       dataDir = "/mnt/nas3/postgresql/16/";
       initdbArgs = ["--data-checksums"];
       enableTCPIP = true;
       settings = {
         wal_level = "replica";
-        archive_mode = "on";
-        archive_command = "${pkgs.wal-g}/bin/wal-g --config ${config.age.secrets."wal-g-config.env".path} wal-push %p";
         shared_preload_libraries = "pg_stat_statements,auto_explain";
         "pg_stat_statements.track" = "all";
         "auto_explain.log_min_duration" = "250ms";
@@ -37,6 +43,37 @@
 
     services.telegraf.extraConfig.inputs.postgresql = {
       address = "postgres://telegraf:$DB_PASSWORD@hallewell/telegraf";
+    };
+
+    services.restic.backups.postgresql = let
+      backupPath = "/mnt/nas3/postgres-backup";
+    in {
+      timerConfig = {
+        OnCalendar = "*-*-* 00/6:00:00";
+        Persistent = true;
+        RandomizedDelaySec = "3h";
+      };
+      repository = "b2:ramona-postgres-backups:/hallewell/";
+      rcloneConfigFile = config.age.secrets."hallewell-postgres-backups-rclone".path;
+      environmentFile = config.age.secrets."hallewell-postgres-backups-env".path;
+      backupPrepareCommand = ''
+        mkdir ${backupPath}
+        chown postgres:postgres ${backupPath}
+        ${pkgs.sudo}/bin/sudo -u postgres ${postgresPackage}/bin/pg_basebackup -Ft -Zzstd -Xstream -D${backupPath}
+      '';
+      backupCleanupCommand = ''
+        rm -r ${backupPath} || true
+      '';
+      passwordFile = config.age.secrets."restic-repository-password.age".path;
+      paths = [
+        backupPath
+      ];
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 4"
+        "--keep-monthly 3"
+        "--keep-yearly 100"
+      ];
     };
   };
 }
