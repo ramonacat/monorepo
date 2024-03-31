88feb0248fc9072ce5df1148a5c8679de4118881
Ramona Łuczkiewicz <ja@agares.info>
2024-03-31 16:33:37 +0200

    all: Allow SSH only through tailscale on all machines

--- modules/installed_base.nix
+++ modules/installed_base.nix
@@ -1,16 +1,18 @@
 {
   lib,
-  modulesPath,
   pkgs,
   ...
 }: {
   config = {
-    services.openssh.enable = true;
-    services.openssh.settings.X11Forwarding = true;
+    services.openssh = {
+      enable = true;
+      openFirewall = false;
+      settings.X11Forwarding = true;
+    };
     users.users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHW4PIqcucwZdFj5u9aMhLj/ernBFV24PyHuspHwh3LT ramona@moonfall"
     ];
-    networking.firewall.allowedTCPPorts = [22];
+    networking.firewall.interfaces.tailscale0.allowedTCPPorts = [22];
     services.fwupd.enable = lib.mkDefault true;
     environment.systemPackages = with pkgs; [pciutils tailscale];
     services.tailscale = {
