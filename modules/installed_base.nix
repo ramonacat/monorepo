be30e7cecf8d63dfde20d9a11c62b2b708f2210e
Ramona Łuczkiewicz <ja@agares.info>
2024-04-12 13:59:18 +0200

    Apparently this is how  multiple keys should be configured?

--- modules/installed_base.nix
+++ modules/installed_base.nix
@@ -29,9 +29,7 @@
       origin = "pam://local";
       debug = true;
       authFile = pkgs.writeText "u2f-auth-file" ''
-        ramona:IfYfQemq431XIiaVdiGFLCxr3UIBD/PH7j4QhBDQBURLgbHwUnoP7xhbPi8xFl46GbBQ9WPInSHlffen8Hi9CQ==,rO6dkgPn/545BgDUHj+hDnvI2wf5R7SeTrrFuqWtOK0Yw8mt+dIxs9Pd8asV6dwEPlVRSWjI8zlnwUKI8PE6uA==,es256,+presence
-        ramona:a4QUXjAdm4kBXoobh9TlTaR5etTFRs99tQDW9sMqL9ugwq61KIU3ow99yz8BaPzCwS/fFiUHiuOk6/HFtPy6WQ==,LWxAOECZfRNuCDbLd7M2UEswECgnlC7KG2I9SXluzM/odVsM4793F17x5Pbckzk0wwwfSqGA0FPK7y9RALbahA==,es256,+presence
-        ramona:owBYel42czlA+alISSGNN4hym6xXBSm0bqowJ6uTpr0m1q9DgfCFPtgbFPhb1+aY6Kw3Xv7BSccvk5Uk5WQ79dYIU3FPN2ixJxDE4g8iwrtHahmAwMpKchp5VhGSfhG2ZnhedpEKYzSp05vfsYZbmuybr3nybfYoP0ZOZu/MAUzFlfLPoEcPTTJV0zUCUG9aOv+gk8Kp6YOxakFf/gY=,VRbZBBGg3qYrjiqmMLbyvelxzhWIlKD3uG324OPn+NpuIW5M7HGIULjzuhB5hq5vrL6kUCEhpjtHvUUUAc67mQ==,es256,+presence
+        ramona:IfYfQemq431XIiaVdiGFLCxr3UIBD/PH7j4QhBDQBURLgbHwUnoP7xhbPi8xFl46GbBQ9WPInSHlffen8Hi9CQ==,rO6dkgPn/545BgDUHj+hDnvI2wf5R7SeTrrFuqWtOK0Yw8mt+dIxs9Pd8asV6dwEPlVRSWjI8zlnwUKI8PE6uA==,es256,+presence:a4QUXjAdm4kBXoobh9TlTaR5etTFRs99tQDW9sMqL9ugwq61KIU3ow99yz8BaPzCwS/fFiUHiuOk6/HFtPy6WQ==,LWxAOECZfRNuCDbLd7M2UEswECgnlC7KG2I9SXluzM/odVsM4793F17x5Pbckzk0wwwfSqGA0FPK7y9RALbahA==,es256,+presence:owBYel42czlA+alISSGNN4hym6xXBSm0bqowJ6uTpr0m1q9DgfCFPtgbFPhb1+aY6Kw3Xv7BSccvk5Uk5WQ79dYIU3FPN2ixJxDE4g8iwrtHahmAwMpKchp5VhGSfhG2ZnhedpEKYzSp05vfsYZbmuybr3nybfYoP0ZOZu/MAUzFlfLPoEcPTTJV0zUCUG9aOv+gk8Kp6YOxakFf/gY=,VRbZBBGg3qYrjiqmMLbyvelxzhWIlKD3uG324OPn+NpuIW5M7HGIULjzuhB5hq5vrL6kUCEhpjtHvUUUAc67mQ==,es256,+presence
       '';
     };
     programs.nix-ld.enable = true;
