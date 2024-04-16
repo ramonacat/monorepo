24b2d9e485761fa6088a8da010aca94081c5c585
Ramona Łuczkiewicz <ja@agares.info>
2024-04-16 16:29:34 +0200

    shadowsoul: sync dls with hallewell

--- machines/hallewell/syncthing.nix
+++ machines/hallewell/syncthing.nix
@@ -7,12 +7,20 @@
 
       settings = {
         devices.seedbox.id = "2SFTGDS-ZURKFAZ-OTUE3XU-7LUQP5T-ZT34A3F-JBASCK5-T5EZOFI-QVBURAM";
+        devices.shadowsoul.id = "7NXR3IB-O4X73UQ-YVL6C5D-WEVRNVZ-5R6MIZH-P73UNPX-LRNJV6K-UEJNUQS";
 
-        folders.shared.path = lib.mkForce "/mnt/nas3/data/shared/";
-        folders.stuff = {
-          id = "4xhsx-qnnqq";
-          path = "/mnt/nas3/stuff/";
-          devices = ["seedbox"];
+        folders = {
+          shared.path = lib.mkForce "/mnt/nas3/data/shared/";
+          stuff = {
+            id = "4xhsx-qnnqq";
+            path = "/mnt/nas3/stuff/";
+            devices = ["seedbox"];
+          };
+          dls = {
+            id = "trnsmsn-dls";
+            path = "/mnt/nas3/dls/";
+            devices = ["shadowsoul"];
+          };
         };
       };
     };
