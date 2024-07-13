f99586d198914094ea1ef6b70b18f8b76f1dbd95
Ramona Łuczkiewicz <ja@agares.info>
2024-07-13 12:54:54 +0200

    users/root: authorize hallwell to connect

--- users/root/base.nix
+++ users/root/base.nix
@@ -3,6 +3,7 @@ _: {
     users.users.root = {
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHW4PIqcucwZdFj5u9aMhLj/ernBFV24PyHuspHwh3LT ramona@moonfall"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBQpyHTzwWTi5x/532hbbsMLCbRBE+icEf1LHNLOyJKF root@hallewell"
       ];
     };
   };
