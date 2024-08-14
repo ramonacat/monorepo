fa76a001ea9356d3c8475d1fec2baee245d97c22
Ramona Łuczkiewicz <ja@agares.info>
2024-08-14 06:40:10 +0200

    ras2: Improve phpfpm config

--- machines/hallewell/ras2.nix
+++ machines/hallewell/ras2.nix
@@ -17,6 +17,8 @@
       "pm.max_children" = 32;
       "pm.max_requests" = 500;
       "pm.start_servers" = 2;
+      "pm.min_spare_servers" = 2;
+      "pm.max_spare_servers" = 5;
       "php_admin_value[error_log]" = "stderr";
       "php_admin_flag[log_errors]" = true;
       "catch_workers_output" = true;
