8d85ed973c6c8cba8116232e6146f173de4ce5c8
Ramona Łuczkiewicz <ja@agares.info>
2024-08-28 13:04:51 +0200

    ras2: fix missing userId crash when starting a task

--- apps/ras2/src/Task/Application/Command/StartWork.php
+++ apps/ras2/src/Task/Application/Command/StartWork.php
@@ -6,6 +6,7 @@ namespace Ramona\Ras2\Task\Application\Command;
 
 use Ramona\Ras2\SharedCore\Infrastructure\CQRS\Command\Command;
 use Ramona\Ras2\SharedCore\Infrastructure\CQRS\Command\ExecutedBy;
+use Ramona\Ras2\SharedCore\Infrastructure\Hydration\HydrateFromSession;
 use Ramona\Ras2\Task\Business\TaskId;
 use Ramona\Ras2\Task\Infrastructure\CommandExecutor\StartWorkExecutor;
 use Ramona\Ras2\User\Business\UserId;
@@ -15,6 +16,7 @@ final readonly class StartWork implements Command
 {
     public function __construct(
         public TaskId $taskId,
+        #[HydrateFromSession('userId')]
         public UserId $userId
     ) {
     }
