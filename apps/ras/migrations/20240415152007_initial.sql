4b553d2da092830ec5f280740af87e8d82921d93
Ramona Łuczkiewicz <ja@agares.info>
2024-09-03 18:45:51 +0200

    ras+rat: Goodbye

--- apps/ras/migrations/20240415152007_initial.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE hosts (
-    hostname TEXT NOT NULL PRIMARY KEY,
-    last_seen TIMESTAMPTZ NOT NULL,
-    running_closure_path TEXT NOT NULL,
-    last_running_closure_change TIMESTAMPTZ NULL
-);
--- apps/rat/src/cli/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod add;
-pub mod calendar;
-pub mod edit;
-pub mod list;
-pub mod maintenance;
-pub mod state_transition;
