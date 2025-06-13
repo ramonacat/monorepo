8a156e3255dc50d4cd3e858c3d4589c6fcf462d7
Ramona Łuczkiewicz <ja@agares.info>
2025-06-13 06:38:05 +0200

    neovim&tmux tweaks

--- users/ramona/neovim/extraConfig.lua
+++ users/ramona/neovim/extraConfig.lua
@@ -100,6 +100,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 require("neo-tree").setup({
 	filesystem = {
+		follow_current_file = {
+			enabled = true,
+		},
 		filtered_items = {
 			always_show = {
 				".github",
