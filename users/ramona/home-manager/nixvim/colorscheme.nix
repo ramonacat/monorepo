f564bb19e5e6dab6787b19eadb9a75019f6b037f
Ramona Łuczkiewicz <ja@agares.info>
2025-07-02 11:42:31 +0200

    nixvim: use terminal background color

--- users/ramona/home-manager/nixvim/colorscheme.nix
+++ users/ramona/home-manager/nixvim/colorscheme.nix
@@ -10,6 +10,12 @@ _: {
         Normal.ctermbg = "none";
         NonText.ctermbg = "none";
       };
+
+      # The highlight option does not accept guibg as a variable for some reason
+      extraConfigVim = ''
+        highlight Normal guibg=none
+        highlight NonText guibg=none
+      '';
     };
   };
 }
