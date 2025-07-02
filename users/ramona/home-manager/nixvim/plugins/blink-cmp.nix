068f28c38c162a6a94601c1e333db7cdc9edc86b
Ramona Łuczkiewicz <ja@agares.info>
2025-07-02 10:22:18 +0200

    nixvim: blink-cmp bugfix

--- users/ramona/home-manager/nixvim/plugins/blink-cmp.nix
+++ users/ramona/home-manager/nixvim/plugins/blink-cmp.nix
@@ -1,11 +1,11 @@
-{config, ...}: {
+_: {
   config = {
     programs.nixvim.plugins.blink-cmp = {
       enable = true;
       settings.keymap = {
         preset = "enter";
         "<C-Space>" = false;
-        "<C-p>" = config.lib.nixvim.mkRaw "function(cmp) cmp.show() end";
+        "<C-p>" = ["show"];
       };
     };
   };
