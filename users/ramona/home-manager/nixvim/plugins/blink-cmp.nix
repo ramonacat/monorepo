c55a1e6baaf2e47094d896399f7bdb9f485f6563
Ramona Łuczkiewicz <ja@agares.info>
2025-07-04 09:06:48 +0200

    nixvim: add rust crates completion

--- users/ramona/home-manager/nixvim/plugins/blink-cmp.nix
+++ users/ramona/home-manager/nixvim/plugins/blink-cmp.nix
@@ -2,11 +2,20 @@ _: {
   config = {
     programs.nixvim.plugins.blink-cmp = {
       enable = true;
-      settings.keymap = {
-        preset = "super-tab";
-        "<C-Space>" = false;
-        "<C-p>" = ["show"];
-        "<CR>" = ["accept" "fallback"];
+      settings = {
+        keymap = {
+          preset = "super-tab";
+          "<C-Space>" = false;
+          "<C-p>" = ["show"];
+          "<CR>" = ["accept" "fallback"];
+        };
+        sources.providers = {
+          crates = {
+            enabled = true;
+            name = "crates";
+            module = "blink.compat.source";
+          };
+        };
       };
     };
   };
