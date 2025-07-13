55b1dea3f7375dea790f13e88db7eff9103de864
Ramona Łuczkiewicz <ja@agares.info>
2025-07-13 15:00:06 +0200

    Setup the atuin server to allow connections from not only localhost

--- machines/hallewell/atuin-server.nix
+++ machines/hallewell/atuin-server.nix
@@ -3,6 +3,7 @@
     services.atuin = {
       enable = true;
       openRegistration = true;
+      host = "0.0.0.0";
     };
 
     networking.firewall.interfaces.tailscale0.allowedTCPPorts = [config.services.atuin.port];
