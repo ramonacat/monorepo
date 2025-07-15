47626f51cc0e0aa95b0cf1d82353d3aff83f2d33
Ramona Łuczkiewicz <ja@agares.info>
2025-07-15 10:40:51 +0200

    Enable all grammars in treesitter

--- users/ramona/home-manager/nixvim/plugins/treesitter.nix
+++ users/ramona/home-manager/nixvim/plugins/treesitter.nix
@@ -1,11 +1,14 @@
-_: {
+{pkgs, ...}: {
   config = {
     programs.nixvim.plugins.treesitter = {
       enable = true;
       settings = {
         highlights.enable = true;
         indent.enable = true;
+        textobjects.enable = true;
       };
+      nixvimInjections = true;
+      grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
     };
   };
 }
