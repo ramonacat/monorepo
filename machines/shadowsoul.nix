e26b2f1a4f6dabe9a68fa387ac334b10de594e36
Ramona Łuczkiewicz <ja@agares.info>
2025-07-26 16:14:39 +0200

    correct configs for syncthing on shadowsoul

--- machines/shadowsoul.nix
+++ machines/shadowsoul.nix
@@ -5,7 +5,6 @@
     ./_notlive.nix
     ./shadowsoul/hardware.nix
     ./shadowsoul/networking.nix
-    ./shadowsoul/syncthing.nix
     ./shadowsoul/transmission.nix
 
     ../users/ramona/installed.nix
