33d0e1624dab664a95f6c3ec1e43db29f0b49dc5
Ramona Łuczkiewicz <ja@agares.info>
2025-07-29 09:45:01 +0200

    correct syncthing config for hallewell

--- machines/_notlive/syncthing.nix
+++ machines/_notlive/syncthing.nix
@@ -2,51 +2,79 @@
   config,
   lib,
   ...
-}: {
-  services.syncthing =
-    if config.networking.hostName == "shadowsoul"
-    then {
+}: let
+  syncthing-gui-port = 8384;
+in {
+  services.syncthing = let
+    paths = import ../../data/paths.nix;
+  in
+    {
       enable = true;
       overrideDevices = true;
       overrideFolders = true;
-      guiAddress = "0.0.0.0:8384";
-      user = "transmission";
-
-      settings = {
-        devices = {
-          hallewell = {id = "WGH223K-BA7PTFL-DG22PJS-DJY3OJP-PGNVTHO-7S6QVAV-ALDTUWY-7NOLXAF";};
-        };
-        folders.dls = {
-          id = "trnsmsn-dls";
-          path = "/var/lib/transmission/Downloads/";
-          devices = ["hallewell"];
-        };
-      };
+      guiAddress = "0.0.0.0:${builtins.toString syncthing-gui-port}";
     }
-    else {
-      enable = true;
-      overrideDevices = true;
-      overrideFolders = true;
-      openDefaultPorts = true;
-      user = "ramona";
-      guiAddress = "0.0.0.0:8384";
+    // (
+      if config.networking.hostName == "shadowsoul"
+      then {
+        user = "transmission";
 
-      dataDir = "/home/ramona/.syncthing-data";
-      configDir = "/home/ramona/.config/syncthing";
-
-      settings = let
-        otherMachineIds = lib.attrsets.filterAttrs (key: _: key != config.networking.hostName) (import ../../data/syncthing-devices-ids.nix);
-      in {
-        devices = lib.attrsets.mapAttrs (_: value: {id = value;}) otherMachineIds;
-
-        folders = {
-          "shared" = {
-            path = "/home/ramona/shared/";
-            devices = lib.attrsets.mapAttrsToList (name: _: name) otherMachineIds;
+        settings = {
+          devices = {
+            hallewell = {id = "WGH223K-BA7PTFL-DG22PJS-DJY3OJP-PGNVTHO-7S6QVAV-ALDTUWY-7NOLXAF";};
+          };
+          folders.dls = {
+            id = "trnsmsn-dls";
+            path = "/var/lib/transmission/Downloads/";
+            devices = ["hallewell"];
           };
         };
-      };
-    };
+      }
+      else if config.networking.hostName == "hallewell"
+      then {
+        user = lib.mkForce "nas";
+        dataDir = lib.mkForce "${paths.hallewell.nas-root}/syncthing/data/";
+        configDir = lib.mkForce "${paths.hallewell.nas-root}/syncthing/config/";
 
-  networking.firewall.interfaces.tailscale0.allowedTCPPorts = [8384];
+        settings = {
+          devices.shadowsoul = {
+            addresses = [
+              "tcp://213.108.112.64:22000"
+            ];
+            id = "7NXR3IB-O4X73UQ-YVL6C5D-WEVRNVZ-5R6MIZH-P73UNPX-LRNJV6K-UEJNUQS";
+          };
+
+          folders = {
+            shared.path = lib.mkForce "${paths.hallewell.nas-share}/ramona/shared/";
+            dls = {
+              id = "trnsmsn-dls";
+              path = "${paths.hallewell.nas-root}/dls/";
+              devices = ["shadowsoul"];
+            };
+          };
+        };
+      }
+      else {
+        openDefaultPorts = true;
+        user = "ramona";
+
+        dataDir = "/home/ramona/.syncthing-data";
+        configDir = "/home/ramona/.config/syncthing";
+
+        settings = let
+          otherMachineIds = lib.attrsets.filterAttrs (key: _: key != config.networking.hostName) (import ../../data/syncthing-devices-ids.nix);
+        in {
+          devices = lib.attrsets.mapAttrs (_: value: {id = value;}) otherMachineIds;
+
+          folders = {
+            "shared" = {
+              path = "/home/ramona/shared/";
+              devices = lib.attrsets.mapAttrsToList (name: _: name) otherMachineIds;
+            };
+          };
+        };
+      }
+    );
+
+  networking.firewall.interfaces.tailscale0.allowedTCPPorts = [syncthing-gui-port];
 }
