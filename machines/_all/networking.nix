9cee066ec4e6aedc39103fcfe5a1e8573433a491
Ramona Łuczkiewicz <ja@agares.info>
2025-07-29 11:04:50 +0200

    migrate from networkmanager to networkd

--- machines/_all/networking.nix
+++ machines/_all/networking.nix
@@ -1,13 +1,10 @@
 {lib, ...}: {
   config = {
-    # this service does nothing useful but breaks rebuilds if it's restarted
-    # https://github.com/NixOS/nixpkgs/issues/180175
-    systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
     networking = {
       useDHCP = lib.mkForce true;
-      networkmanager.enable = true;
       wireless.enable = false;
       nftables.enable = true;
+      useNetworkd = true;
     };
   };
 }
