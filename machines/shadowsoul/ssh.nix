2026e5c46d5a29d06250d8eff01e8f8a8a063994
Ramona Łuczkiewicz <ja@agares.info>
2025-08-02 12:24:27 +0200

    shadowsoul: deploy the ssh keys through an openssh preStart script

--- machines/shadowsoul/ssh.nix
+++ machines/shadowsoul/ssh.nix
@@ -1,5 +1,7 @@
 {config, ...}: {
-  config = {
+  config = let
+    ssh-keys = import ../../data/ssh-keys.nix;
+  in {
     age.secrets = {
       shadowsoul-ssh-host-key-ed25519 = {
         file = ../../secrets/shadowsoul-ssh-host-key-ed25519.age;
@@ -9,21 +11,17 @@
       };
     };
 
-    environment.etc = let
-      ssh-keys = import ../../data/ssh-keys.nix;
-    in {
-      "/etc/ssh/ssh_host_rsa_key.pub" = {
-        text = ssh-keys.machines.shadowsoul-rsa;
-      };
-      "/etc/ssh/ssh_host_ed25519_key.pub" = {
-        text = ssh-keys.machines.shadowsoul-ed25519;
-      };
-      "/etc/ssh/ssh_host_rsa_key" = {
-        source = config.age.secrets.shadowsoul-ssh-host-key-rsa.path;
-      };
-      "/etc/ssh/ssh_host_ed25519_key" = {
-        source = config.age.secrets.shadowsoul-ssh-host-key-ed25519.path;
-      };
-    };
+    systemd.services.openssh.preStart = ''
+      echo <<-EOF
+      ${ssh-keys.machines.shadowsoul-rsa}
+      EOF > /etc/ssh/ssh_host_rsa_key.pub
+
+      echo <<-EOF
+      ${ssh-keys.machines.shadowsoul-ed25519}
+      EOF > /etc/ssh/ssh_host_ed25519_key.pub
+
+      cat ${config.age.secrets.shadowsoul-ssh-host-key-rsa.path} > /etc/ssh/ssh_host_rsa_key
+      cat ${config.age.secrets.shadowsoul-ssh-host-key-ed25519.path} > /etc/ssh/ssh_host_ed25519_key
+    '';
   };
 }
