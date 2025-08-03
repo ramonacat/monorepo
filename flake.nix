b444aa9e1c354c8e0849d04d45a110735ce51cf5
Ramona Łuczkiewicz <ja@agares.info>
2025-08-03 20:25:25 +0200

    lix: use main

--- flake.nix
+++ flake.nix
@@ -11,9 +11,15 @@
       url = "github:ipetkov/crane";
     };
 
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
     };
 
     nixos-generators = {
