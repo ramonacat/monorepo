620be6b3a1d7094fcbdd2914b4658f4a818172b2
Ramona Łuczkiewicz <ja@agares.info>
2025-08-05 18:51:07 +0200

    replace the substituter in ramona's home-manager to the new URL

--- users/ramona/home-manager/wsl.nix
+++ users/ramona/home-manager/wsl.nix
@@ -16,7 +16,7 @@
       gc.automatic = true;
       settings = {
         trusted-public-keys = ["nix-serve--hallewell:U/8IASkklbxXoFqzevYNdIle1xm3G54u9vUSHzmNaik="];
-        substituters = ["http://hallewell:5001/"];
+        substituters = ["https://hallewell.ibis-draconis.ts.net/nix-serve/"];
       };
     };
   };
