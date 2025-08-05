8d41672fa5b4782a9fa92e744771e31f702abb56
Ramona Łuczkiewicz <ja@agares.info>
2025-08-05 18:18:57 +0200

    use non-regexp location for proxy-pass on hallewell

--- machines/hallewell/nginx/host-tailscale/nix-serve.nix
+++ machines/hallewell/nginx/host-tailscale/nix-serve.nix
@@ -1,7 +1,7 @@
 {config, ...}: {
   config = {
     services.nginx.virtualHosts."hallewell.ibis-draconis.ts.net".locations = {
-      "~ /nix-serve/.*" = {
+      "/nix-serve/" = {
         proxyPass = "http://localhost:${builtins.toString config.services.nix-serve.port}/";
       };
     };
