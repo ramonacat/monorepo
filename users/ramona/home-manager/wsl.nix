7c0c79a0c3582840edd728ac165055215cbc3d59
Ramona Łuczkiewicz <ja@agares.info>
2025-08-05 21:14:54 +0200

    update home-manager configs on wsl automatically

--- users/ramona/home-manager/wsl.nix
+++ users/ramona/home-manager/wsl.nix
@@ -1,23 +1,3 @@
-{pkgs, ...}: {
-  config = {
-    programs.bash = {
-      bashrcExtra = ''
-        export SSH_AUTH_SOCK=''${HOME}/.ssh/agent.sock
-        ss -a | grep -q $SSH_AUTH_SOCK
-        if [ $? -ne 0   ]; then
-            rm -f ''${SSH_AUTH_SOCK}
-            ( ${pkgs.util-linux}/bin/setsid ${pkgs.socat}/bin/socat UNIX-LISTEN:''${SSH_AUTH_SOCK},fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
-        fi
-      '';
-    };
-
-    nix = {
-      package = pkgs.nix;
-      gc.automatic = true;
-      settings = {
-        trusted-public-keys = ["nix-serve--hallewell:U/8IASkklbxXoFqzevYNdIle1xm3G54u9vUSHzmNaik="];
-        substituters = ["https://hallewell.ibis-draconis.ts.net/nix-serve/"];
-      };
-    };
-  };
+_: {
+  imports = import ../../../libs/nix/nix-files-from-dir.nix ./wsl;
 }
