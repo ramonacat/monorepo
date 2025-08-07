d224f361f67d4765c916e4e1473e7107aa1970f3
Ramona Łuczkiewicz <ja@agares.info>
2025-08-07 09:33:57 +0200

    generalise syncthing's configuration

--- roles/private/syncthing.nix
+++ roles/private/syncthing.nix
@@ -4,84 +4,54 @@
   ...
 }: let
   syncthing-gui-port = 8384;
-  syncthing-device-ids = import ../../data/syncthing-devices-ids.nix;
-  other-machine-ids = lib.attrsets.filterAttrs (key: _: key != config.networking.hostName) syncthing-device-ids;
-in {
-  services.syncthing = let
-    paths = import ../../data/paths.nix;
-  in
-    {
-      enable = true;
-      overrideDevices = true;
-      overrideFolders = true;
-      guiAddress = "0.0.0.0:${builtins.toString syncthing-gui-port}";
-    }
-    // (
-      if config.networking.hostName == "shadowsoul"
-      then {
-        user = "transmission";
-
-        settings = {
-          devices = {
-            hallewell = {id = syncthing-device-ids.hallewell;};
-          };
-          folders.dls = {
-            id = "trnsmsn-dls";
-            path = paths.shadowsoul.transmission-downloads;
-            devices = ["hallewell"];
-          };
-        };
-      }
-      else if config.networking.hostName == "hallewell"
-      then {
-        user = lib.mkForce "nas";
-        dataDir = lib.mkForce "${paths.hallewell.nas-root}/syncthing/data/";
-        configDir = lib.mkForce "${paths.hallewell.nas-root}/syncthing/config/";
-
-        settings = {
-          devices =
-            (lib.attrsets.mapAttrs (_: value: {id = value;}) other-machine-ids)
-            // {
-              shadowsoul = {
-                addresses = [
-                  "tcp://213.108.112.64:22000"
-                ];
-                id = syncthing-device-ids.shadowsoul;
-              };
-            };
-
-          folders = {
-            shared = {
-              path = lib.mkForce "${paths.hallewell.nas-share}/ramona/shared/";
-              devices = lib.attrsets.mapAttrsToList (name: _: name) other-machine-ids;
-            };
-            dls = {
-              id = "trnsmsn-dls";
-              path = "${paths.hallewell.nas-root}/dls/";
-              devices = ["shadowsoul"];
-            };
-          };
-        };
-      }
-      else {
-        openDefaultPorts = true;
-        user = "ramona";
-
-        dataDir = paths.common.syncthing-data;
-        configDir = paths.common.syncthing-config;
-
-        settings = {
-          devices = lib.attrsets.mapAttrs (_: value: {id = value;}) other-machine-ids;
-
-          folders = {
-            "shared" = {
-              path = paths.common.ramona-shared;
-              devices = lib.attrsets.mapAttrsToList (name: _: name) other-machine-ids;
-            };
-          };
-        };
-      }
+  syncthing-data = import ../../data/syncthing.nix;
+  current-host-topology =
+    lib.mapAttrs
+    (_: hosts: lib.filterAttrs (k: _: k != config.networking.hostName) hosts)
+    (
+      lib.attrsets.filterAttrs
+      (_: v: builtins.hasAttr config.networking.hostName v)
+      syncthing-data.topology
     );
+  enable = current-host-topology != {};
+  settings =
+    syncthing-data.settings.default
+    // (
+      if
+        (
+          builtins.hasAttr config.networking.hostName syncthing-data.settings
+        )
+      then (builtins.getAttr config.networking.hostName syncthing-data.settings)
+      else {}
+    );
+  connected-devices =
+    lib.filterAttrs
+    (k: _: k != config.networking.hostName)
+    (
+      lib.mergeAttrsList (lib.mapAttrsToList (_: machines: machines) current-host-topology)
+    );
+in {
+  services.syncthing = {
+    inherit enable;
+    inherit (settings) user;
+    overrideDevices = true;
+    overrideFolders = true;
+    guiAddress = "0.0.0.0:${builtins.toString syncthing-gui-port}";
+    settings = {
+      devices = lib.mapAttrs (_: id: {inherit id;}) connected-devices;
+      folders =
+        lib.mapAttrs
+        (
+          id: hosts: {
+            inherit id;
+            inherit (settings.folders."${id}") path;
+
+            devices = lib.mapAttrsToList (k: _: k) hosts;
+          }
+        )
+        current-host-topology;
+    };
+  };
 
   networking.firewall.interfaces.tailscale0.allowedTCPPorts = [syncthing-gui-port];
 }
