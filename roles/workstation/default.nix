2f879c2a427dd8277b4d8dc67ade5455ba25982b
Ramona Łuczkiewicz <ja@agares.info>
2025-08-07 18:14:43 +0200

    angelsin: setup wireless network support

--- roles/workstation/default.nix
+++ roles/workstation/default.nix
@@ -1,9 +1,23 @@
-_: {
+{config, ...}: {
   imports = (import ../../libs/nix/nix-files-from-dir.nix ./.) ++ [../../modules/machine-kind.nix];
   config = {
+    age.secrets.wireless-passwords = {
+      file = ../../secrets/wireless-passwords.age;
+    };
     ramona.machine = {
       type = "workstation";
       hasPublicIP = false;
+    };
+    networking.wireless = {
+      enable = true;
+      secretsFile = config.age.secrets.wireless-passwords.path;
+      networks = {
+        Savares = {
+          pskRaw = "ext:Savares";
+          priority = 1000;
+        };
+        graves.pskRaw = "ext:graves";
+      };
     };
   };
 }
