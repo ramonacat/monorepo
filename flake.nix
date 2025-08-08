0e281937a1733ea59e365d0c958ab76c8a94acb4
Ramona Łuczkiewicz <ja@agares.info>
2025-08-08 10:12:32 +0200

    automagically generate homeConfigurations

--- flake.nix
+++ flake.nix
@@ -185,30 +185,42 @@
       ];
       RAMONA_FLAKE_ROOT = ./.;
     };
-    homeConfigurations.ramona = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
+    homeConfigurations = let
+      users = builtins.readDir ./users;
+      configurations = pkgs.lib.flatten (
+        pkgs.lib.mapAttrsToList
+        (
+          user: _: let
+            home-manager-directory = ./users + "/${user}/home-manager";
+          in
+            pkgs.lib.mapAttrsToList
+            (variant: _: {
+              inherit user variant;
+              path = home-manager-directory + "/${variant}";
+            })
+            (
+              if builtins.pathExists home-manager-directory
+              then builtins.readDir home-manager-directory
+              else {}
+            )
+        )
+        users
+      );
+    in
+      pkgs.lib.mergeAttrsList (builtins.map (x: {
+          "${x.user}-${x.variant}" = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            extraSpecialArgs = {
+              inherit inputs;
+              flake = self;
+            };
 
-      extraSpecialArgs = {
-        inherit inputs;
-        flake = self;
-      };
-
-      modules = [
-        ./users/ramona/home-manager/base
-      ];
-    };
-    homeConfigurations.ramona-wsl = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-
-      extraSpecialArgs = {
-        inherit inputs;
-        flake = self;
-      };
-
-      modules = [
-        ./users/ramona/home-manager/wsl
-      ];
-    };
+            modules = [
+              x.path
+            ];
+          };
+        })
+        configurations);
     nixosConfigurations = {
       angelsin-linux = nixpkgs.lib.nixosSystem {
         inherit pkgs;
