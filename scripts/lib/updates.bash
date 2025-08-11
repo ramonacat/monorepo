2f2e28e87771247c96e3dd25a6038df5d3c45e0a
Ramona Łuczkiewicz <ja@agares.info>
2025-08-11 09:58:07 +0200

    updater: tell which hosts are being tried

--- scripts/lib/updates.bash
+++ scripts/lib/updates.bash
@@ -8,6 +8,8 @@ read-closure() {
 	mapfile -t builds_hosts < <(echo "$BUILDERS" | jq --raw-output --compact-output '.[]')
 
 	for builds_host in "${builds_hosts[@]}"; do
+		echo "trying $builds_host..." >&2
+
 		if closure=$(curl --fail --max-time 5 "https://$builds_host.ibis-draconis.ts.net/$path" | tr -d '\n'); then
 			break
 		else
