5ac23c85635c752195c6dc8a3881aa8a340ac768
Ramona Łuczkiewicz <ja@agares.info>
2025-09-02 12:05:15 +0200

    paperless-ngx: setup backups

---
+++ machines/hallewell/paperless/backup.nix
@@ -0,0 +1,16 @@
+{config, ...}: {
+  config = {
+    services.restic.backups.nas = let
+      paths = import ../../../data/paths.nix;
+    in
+      import ../../../libs/nix/mk-restic-config.nix config {
+        timerConfig = {
+          OnCalendar = "*-*-* 00/1:00:00";
+          RandomizedDelaySec = "30m";
+        };
+        paths = [
+          paths.hallewell.paperless
+        ];
+      };
+  };
+}
