2d6964bd7ab055b1e8ed4bec207b955f4516cc74
Ramona Łuczkiewicz <ja@agares.info>
2025-10-05 15:28:38 +0200

    hallewell: don't log telegraf to the local postgres database

--- machines/hallewell/telegraf.nix
+++ machines/hallewell/telegraf.nix
@@ -1,35 +1,8 @@
-{
-  config,
-  lib,
-  ...
-}: {
-  config = let
-    telegraf-tcp-port = 8094;
-  in {
-    age.secrets.telegraf-database = {
-      file = ../../secrets/telegraf-database.age;
-      group = "telegraf";
-      mode = "440";
-    };
-
+{config, ...}: {
+  config = {
     services.telegraf = {
-      environmentFiles = [
-        config.age.secrets.telegraf-database.path
-      ];
       extraConfig = {
-        outputs = lib.mkForce {
-          postgresql = {
-            connection = "postgres://telegraf:$DB_PASSWORD@hallewell/telegraf";
-            timestamp_column_type = "timestamp with time zone";
-            tag_cache_size = 100000;
-          };
-        };
         inputs = {
-          socket_listener = {
-            service_address = "tcp://:${builtins.toString telegraf-tcp-port}";
-            content_encoding = "gzip";
-            data_format = "influx";
-          };
           file = {
             files = ["/var/www/${config.networking.hostName}.ibis-draconis.ts.net/builds/*-closure"];
             data_format = "value";
@@ -40,7 +13,5 @@
         };
       };
     };
-
-    networking.firewall.interfaces.tailscale0.allowedTCPPorts = [telegraf-tcp-port];
   };
 }
