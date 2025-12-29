ef9d736c0086cab23941c6bf451a260270010b4c
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2025-12-29 18:46:28 +0100

    all: add a script to decode kernel stacktraces

--- roles/all/kernel.nix
+++ roles/all/kernel.nix
@@ -1,6 +1,7 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }: {
   config = {
@@ -14,6 +15,23 @@
         features.debug = true;
         sysctl."kernel.sysrq" = 1;
       };
+    };
+    environment = {
+      systemPackages = let
+        inherit (config.boot.kernelPackages) kernel;
+      in [
+        (pkgs.writeShellApplication
+          {
+            name = "decode-kernel-stacktrace";
+            runtimeInputs = with pkgs; [stdenv gcc util-linux];
+            text = ''
+              exec ${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/scripts/decode_stacktrace.sh \
+                  ${kernel.dev}/vmlinux \
+                  /build/source/build/.. \
+                  /run/booted-system/kernel-modules/lib/modules/${kernel.modDirVersion} \
+            '';
+          })
+      ];
     };
   };
 }
