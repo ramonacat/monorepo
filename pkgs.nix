7c18ade4e1cab324d295605a6ab05e7a4d945391
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-01-06 12:26:11 +0100

    bcachefs: update to the latest available dev version
    This should fix reconcile spamming dmesg

--- pkgs.nix
+++ pkgs.nix
@@ -7,6 +7,15 @@
     (import inputs.rust-overlay)
     (_: prev: {
       ramona = prev.lib.mapAttrs (_: v: v.package) local-packages.apps;
+      bcachefs-tools = prev.bcachefs-tools.overrideAttrs (_: {
+        version = "1.35.0-dev";
+        src = prev.fetchFromGitHub {
+          owner = "koverstreet";
+          repo = "bcachefs-tools";
+          rev = "dca1a7bd60e5fb52f172cb793c2fe946f35c86a0";
+          hash = "sha256-I1qaq6zrv4iA/5kL9MmxEEDM2Fk3r37VtptGNyXX03A=";
+        };
+      });
     })
   ];
   pkgsConfig = {
