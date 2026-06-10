87c2d82425b488c3357278cb86a2c7113af4f4ff
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-06-10 14:49:57 +0200

    terraform/k8s: add the CI key to servers

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -16,7 +16,7 @@ module "k8s--darkmore" {
   name          = "darkmore"
   subnet_id     = hcloud_network_subnet.k8s.id
   dns_zone_name = dnsimple_zone.ramona-fun.name
-  ssh_keys      = [hcloud_ssh_key.ramona.id]
+  ssh_keys      = [hcloud_ssh_key.ramona.id, hcloud_ssh_key.ci.id]
   control_plane_nodes = {
     "darkmore-control-plane-0" = { tailscale_tags = split(" ", data.external.tailscale_tags.result["darkmore-control-plane-0"]), private_ipv4 : "10.70.0.10" },
     "darkmore-control-plane-1" = { tailscale_tags = split(" ", data.external.tailscale_tags.result["darkmore-control-plane-1"]), private_ipv4 : "10.70.0.11" },
