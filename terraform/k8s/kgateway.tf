4849cfd5230f62a188d17c2a7e2f4b846638accc
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-06-18 10:22:49 +0200

    kubernetes: deploy kgateway

---
+++ terraform/k8s/kgateway.tf
@@ -0,0 +1,21 @@
+resource "helm_release" "kgateway-crds" {
+  name             = "kgateway-crds"
+  chart            = "oci://cr.kgateway.dev/kgateway-dev/charts/kgateway-crds"
+  namespace        = "kgateway-system"
+  create_namespace = true
+  version          = "v2.3.1"
+}
+
+resource "helm_release" "kgateway" {
+  name             = "kgateway"
+  chart            = "oci://cr.kgateway.dev/kgateway-dev/charts/kgateway"
+  namespace        = helm_release.kgateway-crds.namespace
+  create_namespace = true
+  version          = "v2.3.1"
+
+  values = [yamlencode({
+    controller = {
+      replicaCount = 2
+    }
+  })]
+}
