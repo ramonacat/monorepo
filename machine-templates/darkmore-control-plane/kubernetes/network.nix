ec1778c6fb6e170909cff7dd39f8d139d9baf253
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-06-18 19:07:51 +0200

    kubernetes: really fix network config

--- machine-templates/darkmore-control-plane/kubernetes/network.nix
+++ machine-templates/darkmore-control-plane/kubernetes/network.nix
@@ -1,31 +1,35 @@
 { config, ... }: {
   config = {
-    environment.etc = {
-      "${builtins.replaceStrings ["/etc"] [""] config.ramona.kubernetes.cni.config}10-bridge.conf".text = ''
-        {
-            "cniVersion": "0.2.0",
-            "name": "mynet",
-            "type": "bridge",
-            "bridge": "cni0",
-            "isGateway": true,
-            "ipMasq": true,
-            "ipam": {
-                "type": "host-local",
-                "subnet": "${config.ramona.kubernetes.podCidr}",
-                "routes": [
-                    { "dst": "0.0.0.0/0" }
-                ]
-            }
-        }
-      '';
-      "${config.ramona.kubernetes.cni.config}99-loopback.conf".text = ''
-        {
-            "cniVersion": "0.2.0",
-            "name": "lo",
-            "type": "loopback"
-        }
-      '';
-    };
+    environment.etc =
+      let
+        config-path = builtins.replaceStrings [ "/etc" ] [ "" ] config.ramona.kubernetes.cni.config;
+      in
+      {
+        "${config-path}/10-bridge.conf".text = ''
+          {
+              "cniVersion": "0.2.0",
+              "name": "mynet",
+              "type": "bridge",
+              "bridge": "cni0",
+              "isGateway": true,
+              "ipMasq": true,
+              "ipam": {
+                  "type": "host-local",
+                  "subnet": "${config.ramona.kubernetes.podCidr}",
+                  "routes": [
+                      { "dst": "0.0.0.0/0" }
+                  ]
+              }
+          }
+        '';
+        "${config-path}/99-loopback.conf".text = ''
+          {
+              "cniVersion": "0.2.0",
+              "name": "lo",
+              "type": "loopback"
+          }
+        '';
+      };
 
     networking.firewall.trustedInterfaces = [
       "enp7s0"
