7eb768a70ba769050cd249182559109b67589add
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-06-21 14:16:33 +0200

    kubernetes: deploy longhorn

---
+++ terraform/k8s/longhorn.tf
@@ -0,0 +1,19 @@
+resource "helm_release" "longhorn" {
+  name             = "longhorn"
+  chart            = "longhorn"
+  repository       = "https://charts.longhorn.io"
+  namespace        = "longhorn-system"
+  create_namespace = true
+  version          = "1.12.0"
+
+  values = [yamlencode({
+    httproute = {
+      enabled    = true
+      hostnames  = ["longhorn.infrastructure.ramona.fun"]
+      parentRefs = [{ name = "gateway-tailscale", namespace = "kgateway-system" }]
+      metrics = {
+        serviceMonitor = { enabled = true }
+      }
+    }
+  })]
+}
