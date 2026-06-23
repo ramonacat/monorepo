5aac72d6ff0ee5e848bfccec875a584eb8067d49
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-06-23 14:10:47 +0200

    kubernetes: give postgres more space

--- terraform/k8s/postgres.tf
+++ terraform/k8s/postgres.tf
@@ -28,7 +28,7 @@ resource "helm_release" "cloudnative-pg-database" {
   values = [yamlencode({
     cluster = {
       instances = 3
-      storage   = { size = "2Gi", storageClass = "longhorn" }
+      storage   = { size = "5Gi", storageClass = "longhorn" }
       monitoring = {
         enabled = true
       }
