5b4715bd6a1c3c550c9e5fb89b56f595de4dad19
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-06-24 12:49:57 +0200

    apps/fup: ensure backblaze-b2 really is in PATH

--- packages/fup.nix
+++ packages/fup.nix
@@ -1,11 +1,16 @@
 { pkgs, ... }: rec {
   package = pkgs.stdenvNoCC.mkDerivation {
     name = "fup-1.0.0";
-    buildInputs = with pkgs; [ backblaze-b2 ];
+    nativeBuildInputs = with pkgs; [ makeWrapper ];
     src = ../apps/fup;
     installPhase = ''
       mkdir -p $out/bin
       cp fup $out/bin
+    '';
+
+    postInstall = ''
+      wrapProgram $out/bin/fup \
+          --prefix PATH : ${pkgs.lib.makeBinPath (with pkgs; [ backblaze-b2 ])}
     '';
   };
   coverage = pkgs.runCommand "${package.name}-coverage" { } "echo > $out";
