15e4cdebd0654a182e473b1dc12023550b00452c
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-07-21 15:10:58 +0200

    ci: fix uploads for multiple containers

--- .github/scripts/upload-containers.bash
+++ .github/scripts/upload-containers.bash
@@ -2,15 +2,19 @@
 set -euo pipefail
 
 main() {
+	local image_name
+	local tag
 	local -r now=$(date +%s)
 
 	for image_path in ./containers/*; do
-		local -r image_name=$(basename "$image_path")
-		local -r tag="ghcr.io/ramonacat/$image_name:$now"
+		image_name=$(basename "$image_path")
+		tag="ghcr.io/ramonacat/$image_name:$now"
 
 		docker load --input "$image_path"
 		docker tag "$image_name:latest" "$tag"
 		docker push "$tag"
+
+		echo "pushed: $tag"
 	done
 }
 
