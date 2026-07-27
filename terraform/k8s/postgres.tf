1cb2fdb62c10d639fa43ad1884e73699feb6fb96
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-07-27 21:56:30 +0200

    postgres: switch backups back to b2

--- terraform/k8s/postgres.tf
+++ terraform/k8s/postgres.tf
@@ -68,7 +68,7 @@ resource "helm_release" "cloudnative-pg-database" {
     }
     cluster = {
       instances = 3
-      storage   = { size = "27Gi", storageClass = "hcloud-volumes" }
+      storage   = { size = "28Gi", storageClass = "hcloud-volumes" }
       monitoring = {
         enabled = true
       }
