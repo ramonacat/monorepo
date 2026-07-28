5c6c8812a03ae2fb9863dca0a30627e8ff0d8e64
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-07-28 09:22:35 +0200

    crowdsec: remove
    It's unstable and unpredicatable, and bans enough legitimate traffic to
    be a nuisance. It just creates more problems than it solves.

--- terraform/k8s/kgateway.tf
+++ terraform/k8s/kgateway.tf
@@ -20,26 +20,3 @@ resource "helm_release" "kgateway" {
     }
   })]
 }
-
-resource "helm_release" "envoy-proxy-crowdsec-bouncer" {
-  name             = "envoy-proxy-crowdsec-bouncer"
-  chart            = "oci://ghcr.io/kdwils/charts/envoy-proxy-bouncer"
-  namespace        = "kgateway-system"
-  create_namespace = true
-  version          = "0.6.3"
-
-  values = [yamlencode({
-    replicaCount = 2
-    config = {
-      bouncer = {
-        lapiURL = "http://crowdsec-service.crowdsec:8080"
-        metrics = true
-        apiKeySecretRef = {
-          name = "crowdsec-api-key"
-          key  = "ENVOY_BOUNCER_BOUNCER_APIKEY"
-        }
-      }
-      prometheus = { enabled = true, serviceMonitor = { enabled = true } }
-    }
-  })]
-}
