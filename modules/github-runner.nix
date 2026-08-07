5056e714cba65a9b73b34c4e98b96dc474ea2e9c
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-08-07 23:29:22 +0200

    github-runners: make them nix's trusted users

--- modules/github-runner.nix
+++ modules/github-runner.nix
@@ -29,11 +29,18 @@
       age.secrets.github-pat-runner-registration = {
         file = ../secrets/github-pat-runner-registration.age;
       };
-
+      users.groups.github-runners = { };
+      users.users.github-runners = {
+        isSystemUser = true;
+        group = "github-runners";
+      };
+      nix.settings.trusted-users = [ "github-runners" ];
       services.github-runners = lib.mergeAttrsList (
         map (i: {
           "${config.networking.hostName}-${toString i}" = {
             enable = true;
+            user = "github-runners";
+            group = "github-runners";
             url = "https://github.com/ramonacat/monorepo";
             tokenFile = config.age.secrets.github-pat-runner-registration.path;
             extraLabels = [ "nixos" ];
@@ -51,6 +58,8 @@
           };
           "${config.networking.hostName}-${toString i}-secret" = {
             enable = true;
+            user = "github-runners";
+            group = "github-runners";
             url = "https://github.com/ramonacat/monorepo-secret";
             tokenFile = config.age.secrets.github-pat-runner-registration.path;
             extraLabels = [ "nixos" ];
