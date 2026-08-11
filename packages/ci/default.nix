c40eec017d80ca5555a62f0aed09595d3f9b7aa3
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-08-11 13:20:34 +0200

    ci image: include a setup script

--- packages/ci/default.nix
+++ packages/ci/default.nix
@@ -20,7 +20,7 @@
       bash
       coreutils
       util-linux
-      docker
+      skopeo
       jq
 
       nix
@@ -31,6 +31,29 @@
       openssh
       xz
       iana-etc
+
+      (pkgs.writeShellScriptBin "prepare-ci" ''
+        set -euo pipefail
+
+        mkdir -p /etc/nix/
+        mkdir -p ~/.ssh/
+        mkdir -p ~/.config/rclone/
+
+        attic login main https://attic.infrastructure.ramona.fun/ "$ATTIC_TOKEN"
+
+        echo "extra-experimental-features = flakes nix-command" >> /etc/nix/nix.conf
+        echo "$SSH_KEY" > ~/.ssh/id_ed25519 && chmod 0600 ~/.ssh/id_ed25519 && ssh-keygen -y -f ~/.ssh/id_ed25519 > ~/.ssh/id_ed25519.pub
+
+        echo > ~/.config/rclone/rclone.conf <<-EOT
+        [default]
+        type=s3
+        provider=Hetzner
+        access_key_id=$ACCESS_KEY_ID
+        secret_access_key=$SECRET_ACCESS_KEY
+        region=nbg1
+        endpoint=nbg1.your-objectstorage.com
+        EOT
+      '')
     ];
     extraCommands = ''
       mkdir usr
