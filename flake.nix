4a09d985560205142ab09e4ea6c3ab2bb0981403
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-08-11 22:49:11 +0200

    flake: fix failing checks

--- flake.nix
+++ flake.nix
@@ -70,9 +70,11 @@
 
     pyproject-build-systems = {
       url = "github:pyproject-nix/build-system-pkgs";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.uv2nix.follows = "uv2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        pyproject-nix.follows = "pyproject-nix";
+        uv2nix.follows = "uv2nix";
+        nixpkgs.follows = "nixpkgs";
+      };
     };
   };
 
