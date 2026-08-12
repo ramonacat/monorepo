e1249dc1e6e7267d5586f96e34ed1167a6d7f239
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-08-12 10:43:08 +0200

    github: archive monorepo-secret

--- terraform/github_repo_ramonacat_monorepo-secret.tf
+++ terraform/github_repo_ramonacat_monorepo-secret.tf
@@ -1,7 +1,7 @@
 resource "github_repository" "ramonacat-monorepo-secret" {
   name        = "monorepo-secret"
   visibility  = "private"
-  description = "If you have access to this, you know what kinds of things are here. If you don't and you know me, you probably have a general idea."
+  description = "This repository has been archived. Talk to me to request access to https://code.ramona.fun/ramona/monorepo-secret"
 
   allow_merge_commit = true
   allow_rebase_merge = false
@@ -12,6 +12,8 @@ resource "github_repository" "ramonacat-monorepo-secret" {
   has_issues   = true
   has_projects = false
   has_wiki     = false
+
+  archived = true
 }
 
 resource "github_branch" "ramonacat-monorepo-secret--main" {
@@ -22,23 +24,4 @@ resource "github_branch" "ramonacat-monorepo-secret--main" {
 resource "github_branch_default" "ramonacat-monorepo-secret--main" {
   repository = github_repository.ramonacat-monorepo-secret.name
   branch     = github_branch.ramonacat-monorepo-secret--main.branch
-}
-
-resource "github_actions_repository_permissions" "ramonacat-monorepo-secret" {
-  repository      = github_repository.ramonacat-monorepo-secret.name
-  allowed_actions = "all"
-}
-
-resource "tailscale_tailnet_key" "monorepo-secret-ci" {
-  ephemeral           = true
-  reusable            = true
-  recreate_if_invalid = "always"
-  preauthorized       = true
-  tags                = ["tag:ci"]
-}
-
-resource "github_actions_secret" "ramonacat-monorepo-s-ecret--argo-tailscale-token" {
-  repository  = github_repository.ramonacat-monorepo-secret.id
-  secret_name = "TAILSCALE_AUTHKEY"
-  value       = tailscale_tailnet_key.monorepo-secret-ci.key
 }
