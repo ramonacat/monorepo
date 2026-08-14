c72171ca142c3bce01030058482cdae408411bfa
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-08-15 00:12:57 +0200

    ci: use woodpecker

---
+++ .woodpecker/scripts/build.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+declare ATTIC_PID=""
+
+cleanup() {
+	if [[ "$ATTIC_PID" != "" ]]; then
+		kill $ATTIC_PID || true
+	fi
+}
+
+main() {
+	ci setup
+	ci cache pull terraform
+
+	attic watch-store main &
+	ATTIC_PID=$!
+	trap cleanup EXIT
+
+	ci check
+
+	nix build .#everything --fallback
+
+	if [[ "${CI_COMMIT_SOURCE_BRANCH:-${CI_COMMIT_BRANCH:-main}}" == "main" ]]; then
+		echo "on the main branch, publishing changed containers"
+
+		ci publish
+	fi
+
+	ci cache push terraform terraform/.terraform
+}
+
+main
--- machines/iso/default.nix
+++ machines/iso/default.nix
@@ -1,7 +1,5 @@
 { inputs, ... }: {
   imports = [
-    inputs.nixos-generators.nixosModules.all-formats
-
     "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
     ../../roles/all
 
@@ -11,5 +9,6 @@
   ];
   config = {
     ramona.machine.type = "live";
+    isoImage.edition = "ramona";
   };
 }
