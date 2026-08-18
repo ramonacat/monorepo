605dace8895ee0ef25915077ffa777f28f742201
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-08-18 13:43:31 +0200

    switch all containers and npm to github

--- users/ramona/home-manager/workstation/npm.nix
+++ users/ramona/home-manager/workstation/npm.nix
@@ -1,19 +1,23 @@
-{ config, ... }:
+_:
 let
   mutable-npm-path = "~/.mutable_nodejs_modules";
 in
 {
-  age.secrets.github-pat-npm-registry.file = ../../../../secrets/github-pat-npm-registry.age;
+  age.secrets.cli-tokens.file = ../../../../secrets/cli-tokens.age;
 
   home = {
     file.".npmrc".text = ''
       prefix = ${mutable-npm-path}
       @ramonacat:registry=https://npm.pkg.github.com
+      @ramona:registry=https://code.ramona.fun/api/packages/ramona/npm
+      //code.ramona.fun/api/packages/ramona/npm/:_authToken=''${FORGEJO_TOKEN}
       //npm.pkg.github.com/:_authToken=''${GITHUB_TOKEN}
     '';
     sessionPath = [ "${mutable-npm-path}/bin/" ];
     sessionVariablesExtra = ''
-      export GITHUB_TOKEN=$(cat ${config.age.secrets.github-pat-npm-registry.path})
+      set -a
+      eval "$(cat {config.age.secrets.cli-tokens.path})" >/dev/null
+      set +a
     '';
   };
 }
