a4bbe8709d23b680ddf768f6770a75e3d3fe0aac
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-08-22 10:01:43 +0200

    rapp: reformat with ktfmt

--- apps/rapp/app/src/main/java/fun/ramona/rapp/MainActivity.kt
+++ apps/rapp/app/src/main/java/fun/ramona/rapp/MainActivity.kt
@@ -14,34 +14,34 @@ import androidx.compose.ui.tooling.preview.Preview
 import `fun`.ramona.rapp.ui.theme.RappTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            RappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      RappTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          Greeting(
+              name = "Android",
+              modifier = Modifier.padding(innerPadding),
+          )
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+  Text(
+      text = "Hello $name!",
+      modifier = modifier,
+  )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    RappTheme {
-        Greeting("Android")
-    }
+  RappTheme {
+    Greeting("Android")
+  }
 }
