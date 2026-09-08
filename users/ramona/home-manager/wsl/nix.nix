2b8c56b9f40b426151b4be984a3c03bc3c6468d2
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-09-08 16:37:07 +0200

    nix: allow cache signed by attic.home.ramona.fun

--- users/ramona/home-manager/wsl/nix.nix
+++ users/ramona/home-manager/wsl/nix.nix
@@ -30,6 +30,7 @@
           "nix-serve--hallewell:U/8IASkklbxXoFqzevYNdIle1xm3G54u9vUSHzmNaik="
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "main:Ijh1gpf5zuqCEsdfP6nBeGLg+/v+9SW7T3+cS81TqW4="
+          "main:3C+LexoJwRwF2lN6ZHHQsYsObdBt5wjPxyLZhySIyEE="
         ];
         substituters = [
           "https://cache.nixos.org/"
