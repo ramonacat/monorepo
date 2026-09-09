2bfc4429dfc83ef9f7d981256551782b78cb4362
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-09-09 22:44:35 +0200

    home-network: setup qos

---
+++ terraform/network-home/scarletwound_ports.tf
@@ -0,0 +1,10 @@
+resource "routeros_interface_ethernet" "scarletwound-ether" {
+  provider = routeros.router-scarletwound
+  count = 5
+
+  factory_name = "ether${count.index+1}"
+  name = "ether${count.index+1}"
+  tx_flow_control = "on"
+  rx_flow_control = "on"
+  l2mtu = 1598
+}
---
+++ terraform/network-home/scarletwound_qos.tf
@@ -0,0 +1,18 @@
+resource "routeros_queue_tree" "scarletwound-vlan4" {
+  provider = routeros.router-scarletwound
+  name = "vlan4"
+  parent = module.scarletwound-vlan4.vlan_interface
+  priority = 6
+}
+
+resource "routeros_queue_tree" "scarletwound-vlan3" {
+  provider = routeros.router-scarletwound
+  name = "vlan3"
+  parent = routeros_interface_vlan.scarletwound-vlan3.name
+}
+
+resource "routeros_queue_tree" "scarletwound-vlan7" {
+  provider = routeros.router-scarletwound
+  name = "vlan7"
+  parent = routeros_interface_vlan.scarletwound-vlan7.name
+}
