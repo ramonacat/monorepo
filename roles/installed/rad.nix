d27da0ad65ea7bed6b20cae808140ea90ba3fd0a
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-09-20 19:29:14 +0200

    rad: ensure rad can enumerate public ips

--- roles/installed/rad.nix
+++ roles/installed/rad.nix
@@ -54,6 +54,8 @@
       };
       serviceConfig = {
         ExecStart = "${pkgs.ramona.rad}/bin/rad";
+        AmbientCapabilities = "CAP_NET_RAW";
+        CapabilityBoundingSet = "CAP_NET_RAW";
       };
     };
   };
